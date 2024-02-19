ff6366ae6cf49a0ccc1bfe2f3cff49277f8cfafb
bk91062 <bk91062@DESKTOP-GBCJOIE>
2024-02-19 18:33:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            char operation;
+
+            Console.Write("Enter the first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the operator (+, -, /, *): ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine($"{num1} / {num2} = {(double)num1 / num2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: Unsupported operator.");
+                    break;
+            }
+        }
+    }
+}
